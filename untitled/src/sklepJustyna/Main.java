16c79d8858bfe096c00172861cc565222c8fdc18
Justyna Duranowska <justa8866@gmail.com>
2022-05-28 20:32:33 +0200

    change c# to java

---
+++ untitled/src/sklepJustyna/Main.java
@@ -0,0 +1,22 @@
+package sklepJustyna;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/Sklep",
+                            "postgres", "zabicdude");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+}
